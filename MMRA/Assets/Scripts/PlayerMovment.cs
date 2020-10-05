cb1b9a406c2ef6c13e2eb5f5c31a301a3a6787d7
sandint2011 <sandint2011@my.uwstout.edu>
2020-10-05 13:18:43 -0500

    Added jump mechanics to player object
    Added jumping to the player object using the PlayerMovement script.

--- MMRA/Assets/Scripts/PlayerMovment.cs
+++ MMRA/Assets/Scripts/PlayerMovment.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
     public float movementSpeed;
     public Rigidbody2D rb;
+
+    public float jumpForce = 20f;
+    public Transform feet;
+    public LayerMask groundLayers;
 
     private float mx; // Movement on the x-axis
 
@@ -19,12 +24,39 @@ public class PlayerMovment : MonoBehaviour
     void Update()
     {
         mx = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            jump();
+        }
     }
 
     private void FixedUpdate()
     {
-        // Grab current velocity on y-axis
+        // Add movement vector
         Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
         rb.velocity = movement;
+    }
+
+    private void jump()
+    {
+        // Add jump vector
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
+    }
+
+    public bool isGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+        
+        if (groundCheck != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
