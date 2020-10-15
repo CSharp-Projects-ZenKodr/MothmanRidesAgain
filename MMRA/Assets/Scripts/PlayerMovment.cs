6cea62df61824eb284bd099494e04cd464559738
Trusty04 <trustygaming04@gmail.com>
2020-10-15 10:03:06 -0500

    Sprint 2
    Protype Level
    Physics
    Sprites for Mothman, ground, objects

--- MMRA/Assets/Scripts/PlayerMovment.cs
+++ MMRA/Assets/Scripts/PlayerMovment.cs
@@ -9,11 +9,14 @@ public class PlayerMovment : MonoBehaviour
     public int slopeSpeed = 10;
     public Rigidbody2D rb;
 
+    public Animator anim;
+
     public float jumpForce = 20f;
     public Transform feet;
     public LayerMask groundLayers;
+    
 
-    private float mx; // Movement on the x-axis
+    private float inputX; // Movement on the x-axis
 
     // Start is called before the first frame update
     void Start()
@@ -24,34 +27,41 @@ public class PlayerMovment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mx = Input.GetAxisRaw("Horizontal");
+        inputX = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
             jump();
         }
+
+        anim.SetBool("isGrounded", isGrounded());
+
+        if (inputX > 0f)
+            transform.localScale = new Vector3(2f, 2f, 2f);
+        else if (inputX < 0f)
+            transform.localScale = new Vector3(-2f, 2f, 2f);
     }
 
     private void FixedUpdate()
     {
         // Add movement vector
-        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
-        rb.velocity = movement;
+        Vector2 mx = new Vector2(inputX * movementSpeed, 0);
+        rb.AddForce(mx);
+
     }
 
     private void jump()
     {
         // Add jump vector
-        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
-
-        rb.velocity = movement;
+        Vector2 my = new Vector2(0, jumpForce);
+        rb.AddForce(my, ForceMode2D.Impulse);
     }
-    
+
     public bool isGrounded()
     {
         // Check for collision with objects in Ground layer
         Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
-        
+
         if (groundCheck != null)
         {
             return true;
