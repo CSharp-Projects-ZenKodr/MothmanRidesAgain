20ee3d49c3877d64ee47c886e280ad9f5293af4b
Trusty04 <trustygaming04@gmail.com>
2020-11-03 09:11:25 -0600

    Fix

---
+++ MMRA/Assets/Scripts/TimeTracker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeTracker : MonoBehaviour
+{
+    public float levelTime = 180;
+    private Text timeDisplay;
+    private int min, sec;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeDisplay = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        min = (int) (levelTime / 60);
+        sec = (int) (levelTime % 60);
+
+        Debug.Log(min + ":" + sec);
+        if (sec < 10)
+            timeDisplay.text = (min + ":0" + sec);
+        else
+            timeDisplay.text = (min + ":" + sec);
+
+        levelTime -= Time.deltaTime;
+    }
+
+}
