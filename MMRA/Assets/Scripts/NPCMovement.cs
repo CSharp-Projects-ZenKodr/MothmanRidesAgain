8d27d9b921b508d6aeaed8af39fc20b8d171af02
BHaaning <haaningb2237@my.uwstout.edu>
2020-11-03 19:00:47 -0600

    Changes to Rails

---
+++ MMRA/Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class NPCMovement : MonoBehaviour
+{
+
+    public float speed;
+    public bool MoveRight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MoveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.gameObject.CompareTag("turn"))
+        {
+            if (MoveRight)
+            {
+                MoveRight = false;
+            }
+            else
+            {
+                MoveRight = true;
+            }
+        }
+    }
+}
