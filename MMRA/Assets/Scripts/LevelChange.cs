ef5ac004540c11a1a6d768ffe44883d3092f5361
Trusty04 <trustygaming04@gmail.com>
2020-11-10 09:01:54 -0600

    Added Level Changer
    Added a script that when the player reaches the end they go to level 2.
    I also moved the hills around in the background.

---
+++ MMRA/Assets/Scripts/LevelChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour
+{
+    public int index;
+
+   void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            //Loads scene using its index in Build Settings
+            SceneManager.LoadScene(index);
+        }
+    }
+}
