f06158fe35ae20e9076f2ca9c93663030cb84cb4
BHaaning <haaningb2237@my.uwstout.edu>
2020-11-17 08:22:09 -0600

    Added enemy with placeholder sprite.
    Enemy Can be duplicated and placed as deemed necessary for testing  and level design purposes.
    
    TODO for Brad
    Add sprite from assets
    Add vision cone and detection behavior

---
+++ MMRA/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 2f;
+    public Rigidbody2D rb;
+    public LayerMask groundLayers;
+
+    public Transform groundCheck;
+    bool isfacingRight = true;
+
+    RaycastHit2D hit;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hit = Physics2D.Raycast(groundCheck.position, -transform.up, 1f, groundLayers);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (hit.collider != false)
+        {
+            if (isfacingRight)
+            {
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            }
+            else
+            {
+                rb.velocity = new Vector2(-speed, rb.velocity.y);
+            }
+        }
+        else
+        {
+            isfacingRight = !isfacingRight;
+            transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);
+        }
+    }
+}
---
+++ MMRA/Assets/Scripts/LineOfSight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 targetDir = target.position - transform.position;
+        Vector3 forward = transform.forward;
+        float angle = Vector3.Angle(targetDir - transform.position, transform.forward);
+        var dist = Vector3.Distance(target.position, transform.position);
+        if (angle < 22.5 && dist < 90)
+        {
+            Debug.Log("Detected");
+        }
+        else
+        {
+            Debug.Log("Not Detected");
+        }
+    }
+}
